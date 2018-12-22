3f17045c9195802225850997a0d0dbbda806a73b
Baldeep Singh <38287537+itsbaldeep@users.noreply.github.com>
2018-12-22 17:24:39 +0530

    Updated comments #2
    Comments were carried over from Javascript and were not changed according to Python. Now fixed

--- smallest_num_from_list.py
+++ smallest_num_from_list.py
@@ -1,8 +1,5 @@
 # Let an list of some random numbers
 list = [12, 13, 14, 11, 15]
 
-# Using the spread operator(...)
-# We pass in the elements as arguments in min method of Math object
+# Print the min value of the list
 print(min(list))
-
-# It logs the smallest number from the list
