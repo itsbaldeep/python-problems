62836141b3c70c72ef9299b6c35b958defced557
Baldeep Singh <38287537+itsbaldeep@users.noreply.github.com>
2018-12-22 17:24:03 +0530

    Updated comments
    Comments were carried over from Javascript and were not changed according to Python. Now fixed

--- biggest_num_from_list.py
+++ biggest_num_from_list.py
@@ -1,8 +1,5 @@
 # Let an list of some random numbers
 list = [12, 13, 14, 11, 15]
 
-# Using the spread operator(...)
-# We pass in the elements as arguments in max method of Math object
+# Print the max element from list
 print(max(list))
-
-# It logs the biggest number from the list
